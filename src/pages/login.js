9a26a80f64888bb47f28a897c97d9346db40d29f
DoubleCheese <zhuzhening1111@sina.com>
2023-02-13 21:38:02 +0000

    fix: figure display with large height

---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './login.css';
+import { Grid, Card, Button, RadioGroup, FormControlLabel, Radio, TextField, CircularProgress, Checkbox } from '@mui/material';
+import { Stack } from '@mui/system';
+
+class Login extends React.Component {
+    render() {
+        document.title = "Login";
+        return (
+            <div>login</div>
+        )
+    }
+}
+
+export default Login;
