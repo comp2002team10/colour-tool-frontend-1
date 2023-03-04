4c34f8e07bb191d32f6a3aa26f1cd5e56206e37e
nita-kras <nita.krasnita@gmail.com>
2023-03-04 20:42:28 +0000

    Exploration Filter Menu Fix

--- src/pages/exploration.js
+++ src/pages/exploration.js
@@ -2,56 +2,27 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import Checkbox from '@mui/material/Checkbox';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+
 
 class Exploration extends React.Component {
-
-  constructor(props) {
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-}
-
-    render() {
-        return (
-            <div className="App">
-                 <Button
-        id="demo-positioned-button"
-        aria-controls={open ? 'demo-positioned-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        Filter Images
-      </Button>
-      <Menu
-        id="demo-positioned-menu"
-        aria-labelledby="demo-positioned-button"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <MenuItem> <Checkbox {...label} label = "colour"/></MenuItem>
-        <MenuItem> <Checkbox {...label} label = "black and white"/></MenuItem>
-        <MenuItem> <Checkbox {...label} label = "greyscale"/></MenuItem>
-      </Menu>
-
-            
-            </div>
-        );
-    }
-
+  render() {
+    document.title = "Exploration tool";
+    return (
+      <PopupState variant="popover" popupId="demo-popup-menu">
+        {(popupState) => (
+          <React.Fragment>
+            <Button variant="contained" {...bindTrigger(popupState)}>
+              Dashboard
+            </Button>
+            <Menu {...bindMenu(popupState)}>
+              <MenuItem onClick={popupState.close}>Profile</MenuItem>
+              <MenuItem onClick={popupState.close}>My account</MenuItem>
+              <MenuItem onClick={popupState.close}>Logout</MenuItem>
+            </Menu>
+          </React.Fragment>
+        )}
+      </PopupState>
+    );
+  }
 }
